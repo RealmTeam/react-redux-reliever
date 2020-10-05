a711d318a84a5d12bf442291933b1b346ab696e1
Philip Garnero <philip.garnero@seelk.co>
2020-10-05 16:20:40 +0200

    v3.0.2

--- src/utils/merger.js
+++ src/utils/merger.js
@@ -3,12 +3,19 @@ import Immutable from 'seamless-immutable'
 export const DEL = '__DEL__'
 export const OVERWRITE = '__OVERWRITE__'
 
+// https://github.com/rtfeldman/seamless-immutable/blob/2d870b14a01e222493c686a7644181185f859558/src/seamless-immutable.js#L75
+function isMergableObject(target) {
+  return (target !== null && typeof target === "object"
+          && !(Array.isArray(target)) && !(target instanceof Date))
+}
+
 // https://stackoverflow.com/questions/5876332/how-can-i-differentiate-between-an-object-literal-other-javascript-objects/5878101#5878101
 function isPlainObject(obj) {
   if (typeof obj == 'object' && obj !== null) {
     if (typeof Object.getPrototypeOf == 'function') {
       var proto = Object.getPrototypeOf(obj)
-      return proto === Object.prototype || proto === null
+      return (proto === Object.prototype || proto === null ||
+              (Immutable.isImmutable(obj) && isMergableObject(obj)))
     }
     return Object.prototype.toString.call(obj) == '[object Object]'
   }
@@ -36,7 +43,8 @@ export default function merger(a, b) {
   }
 
   // Regular merge
-  if (a != null && Immutable.isImmutable(a) && !Array.isArray(a) && b != null && isPlainObject(b)) {
+  if (a != null && isPlainObject(a) && !Array.isArray(a) &&
+      b != null && isPlainObject(b)) {
     return Immutable.merge(a, b, {deep: true, merger})
   }
   return b
